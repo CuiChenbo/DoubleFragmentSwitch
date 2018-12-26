98eabdb7b5dfb3c08b426e38fa9bce8eee4755a1
ChenboCui <891789193@qq.com>
2018-12-26 10:29:51 +0800

    注释

--- app/src/main/java/www/ccb/com/doublefragmentswitch/ContinueSlideScrollView.java
+++ app/src/main/java/www/ccb/com/doublefragmentswitch/ContinueSlideScrollView.java
@@ -15,14 +15,28 @@ public class ContinueSlideScrollView extends ScrollView {
     private boolean isScrolledToTop = true;// 初始化的时候设置一下值
     private boolean isScrolledToBottom = false;
     private Context context;
+
+
+    private ISmartScrollChangedListener mSmartScrollChangedListener;
+
+    public ContinueSlideScrollView(Context context) {
+        super(context);
+        this.context = context;
+    }
+
     public ContinueSlideScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
     }
 
-    private ISmartScrollChangedListener mSmartScrollChangedListener;
+    public ContinueSlideScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+    }
 
-    /** 滑动到顶部和底部的监听接口 */
+    /**
+     * 滑动到顶部或底部的监听接口
+     * */
     public interface ISmartScrollChangedListener {
         void onScrolledToBottom();
         void onScrolledToTop();
@@ -49,12 +63,10 @@ public class ContinueSlideScrollView extends ScrollView {
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
         if (android.os.Build.VERSION.SDK_INT < 9) {  // API 9及之后走onOverScrolled方法监听
-            if (getScrollY() == 0) {    // 小心踩坑1: 这里不能是getScrollY() <= 0
+            if (getScrollY() == 0) {
                 isScrolledToTop = true;
                 isScrolledToBottom = false;
             } else if (getScrollY() + getHeight() - getPaddingTop()-getPaddingBottom() == getChildAt(0).getHeight()) {
-                // 小心踩坑2: 这里不能是 >=
-                // 小心踩坑3（可能忽视的细节2）：这里最容易忽视的就是ScrollView上下的padding　
                 isScrolledToBottom = true;
                 isScrolledToTop = false;
             } else {
@@ -63,11 +75,6 @@ public class ContinueSlideScrollView extends ScrollView {
             }
             notifyScrollChangedListeners();
         }
-        //为了兼容一些边界奇葩情况，上面的代码就会写成<=,>=的情况，结果就出bug了
-        // 写成这样：getScrollY() + getHeight() >= getChildAt(0).getHeight()
-        // 结果发现快滑动到底部但是还没到时，会发现上面的条件成立了，导致判断错误
-        // 原因：getScrollY()值不是绝对靠谱的，它会超过边界值，但是它自己会恢复正确，导致上面的计算条件不成立
-        // 仔细想想也感觉想得通，系统的ScrollView在处理滚动的时候动态计算那个scrollY的时候也会出现超过边界再修正的情况
     }
 
     private void notifyScrollChangedListeners() {
@@ -96,7 +103,7 @@ public class ContinueSlideScrollView extends ScrollView {
      */
     private float down,move;
     private boolean isExecute = false; //是否要执行
-    private int TRIGGER_DISTANCE = 160;  //触发间距
+    private int TRIGGER_DISTANCE = 160;  //继续滑动效果触发间距 dp
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         switch (ev.getAction()){
